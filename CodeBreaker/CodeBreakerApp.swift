5a4ec451f7e55a2fa7d9d481a53e1b338a8074d8
Jianhua Huang <jayfavored@outlook.com>
2026-06-18 22:13:08 -0400

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Jay Huang on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
